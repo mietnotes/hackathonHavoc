14c8232b6d3e35050a47b3594d240867fd6d4df5
Tejus Gupta <tejus3131@gmail.com>
2023-06-08 14:51:57 +0530

    basic commit

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
+
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/hackathonHavoc")
-def about():
-    return render_template("hackathonHavoc.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
